d24be58138b7b1f5170a5de19e49e45e119ead6e
Natsu Ando <s21c1008re@s.chibakoudai.jp>
2022-10-27 13:04:43 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Natsu Ando
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,16 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRAGE INPUT###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
 
 [ "$res" = 0 ] && echo OK
 exit $res
